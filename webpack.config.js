86c9a3424acff34e22c0f25163d9d8e621bdbc64
vilvaathibanpb <vilvaathiban@gmail.com>
2023-01-28 22:02:06 +0100

    fix: inital commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.jsx",
   output: {
     path: path.join(__dirname, "/dist"), // the bundle output path
     filename: "bundle.js", // the name of the bundle
@@ -33,5 +34,8 @@ module.exports = {
         options: { limit: false },
       },
     ],
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
   },
 };
