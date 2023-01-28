b621214cf4ce15403375bf5db3fd7a1314565779
vilvaathibanpb <vilvaathiban@gmail.com>
2023-01-28 21:58:22 +0100

    fix: initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Text from "./Text";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Text />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Text.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    <p>
+      This is a simple react component to build custom Webpack and Babel plugin
+    </p>
+  );
+};
+
+export default Text
